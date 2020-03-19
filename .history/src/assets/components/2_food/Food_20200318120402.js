e280e328c9adcc8f867fbe5acb00377ef899a0ab
DaNiels89 <nielssloesen@hotmail.com>
2020-03-18 21:03:39 -0500

    Removed git history folder

--- .history/src/assets/components/2_food/Food_20200318120402.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Food = () => {
-  return <div className="Food"></div>;
-};
--- .history/src/assets/components/2_food/Food_20200318120421.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default const Food = () => {
-  return <div className="Food"></div>;
-};
-
